99f51b2c6d04552555104d610ae44785f48bb5de
Chungus McGee <106646061+chungus-mcgee@users.noreply.github.com>
2022-12-23 12:59:01 -0800

    remove need for confirming replacement of file

--- edit.ps1
+++ edit.ps1
@@ -2,10 +2,13 @@
 $currentDir = $MyInvocation.MyCommand.Path | Split-Path -Parent
 cd $currentDir
 $corefilename = "pws.csv"
+$tmpfileanme = "pwsbkp.csv.gpg"
 $encryptedfilename = "pws.csv.gpg"
 Write-Output "Decrypting..."
 gpg -o $corefilename -d $encryptedfilename
 notepad.exe $corefilename | Out-Null
 Write-Output "Encrypting..."
-gpg -c $corefilename
+gpg -o $tmpfileanme -c $corefilename
 rm $corefilename
+rm $encryptedfilename
+mv $tmpfileanme $encryptedfilename
